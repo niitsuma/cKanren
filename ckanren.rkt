de15cd3213f11a8d7a680628a7a2ef1fd701e789
Ramakrishnan Muthukrishnan <vu3rdd@gmail.com>
2012-01-06 23:20:55 +0530

    add more modules into the repl

--- ckanren.rkt
+++ ckanren.rkt
@@ -1,5 +1,9 @@
 #lang racket
 
 (require "ck.scm"
+         "mk.scm"
          "tree-unify.scm"
-         "fd.scm")
+         "fd.scm"
+         "miniKanren.scm")
+
+(provide (all-from-out "ck.scm" "mk.scm" "tree-unify.scm" "fd.scm" "miniKanren.scm"))
