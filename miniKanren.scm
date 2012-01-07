0b6bb3ef8dfc101fbaa4845e699d5b99ed139bd9
Ramakrishnan Muthukrishnan <vu3rdd@gmail.com>
2012-01-07 14:11:42 +0530

    export all defined functions.

--- miniKanren.scm
+++ miniKanren.scm
@@ -5,7 +5,7 @@
          "ck.scm"
          "tree-unify.scm")
 
-(provide caro cdro)
+(provide (all-defined-out))
 
 (define-syntax run1 (syntax-rules () ((_ (x) g0 g ...) (run 1 (x) g0 g ...))))
 (define-syntax run2 (syntax-rules () ((_ (x) g0 g ...) (run 2 (x) g0 g ...))))
