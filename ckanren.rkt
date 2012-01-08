ba35c22d6ac0f078f22f9094346f7a46e9cdd77a
Ramakrishnan Muthukrishnan <vu3rdd@gmail.com>
2012-01-08 23:07:47 +0530

    cKanren as a module language

--- ckanren.rkt
+++ ckanren.rkt
@@ -6,4 +6,5 @@
          "fd.scm"
          "miniKanren.scm")
 
-(provide (all-from-out "ck.scm" "mk.scm" "tree-unify.scm" "fd.scm" "miniKanren.scm"))
+(provide (all-from-out "ck.scm" "mk.scm" "tree-unify.scm" "fd.scm" "miniKanren.scm")
+         (all-from-out racket))
