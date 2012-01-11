5d175f5439577f25e77bfa3917f7ecc86e9b34de
Ramakrishnan Muthukrishnan <vu3rdd@gmail.com>
2012-01-11 13:27:05 +0530

    add never-true into cKanren

--- ckanren.rkt
+++ ckanren.rkt
@@ -5,7 +5,8 @@
          "tree-unify.scm"
          "fd.scm"
          "miniKanren.scm"
-         "neq.scm")
+         "neq.scm"
+         "never-true.scm")
 
-(provide (all-from-out "ck.scm" "mk.scm" "tree-unify.scm" "fd.scm" "miniKanren.scm" "neq.scm")
+(provide (all-from-out "ck.scm" "mk.scm" "tree-unify.scm" "fd.scm" "miniKanren.scm" "neq.scm" "never-true.scm")
          (all-from-out racket))
