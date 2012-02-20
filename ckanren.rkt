68105405335c7acadb2cb6b8a390dbc7b87d0f37
Ramakrishnan Muthukrishnan <vu3rdd@gmail.com>
2012-02-20 22:34:32 +0100

    add tracing.scm to the cKanren lang

--- ckanren.rkt
+++ ckanren.rkt
@@ -6,7 +6,8 @@
          "fd.scm"
          "miniKanren.scm"
          "neq.scm"
-         "never-true.scm")
+         "never-true.scm"
+         "tracing.scm")
 
-(provide (all-from-out "ck.scm" "mk.scm" "tree-unify.scm" "fd.scm" "miniKanren.scm" "neq.scm" "never-true.scm")
+(provide (all-from-out "ck.scm" "mk.scm" "tree-unify.scm" "fd.scm" "miniKanren.scm" "neq.scm" "never-true.scm" "tracing.scm")
          (all-from-out racket))
